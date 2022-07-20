13b4d1160618acfdbdd8f0864904e98ee8dcc32c
Oluwafemi <oloyedeoluwafemi22@gmail.com>
2022-07-20 19:44:03 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,12 +15,14 @@ export const Home = ({ themeValue, data}) => {
           console.log(data)
         })
     }, [data])
+
+    const [search, setSearch] = useState()
     
   return (
     <div className='mx-10 w-screen mt-2 py-2'>
         <div className="flex justify-between drop-shadow rounded">
           <span className={themeValue ? "flex self-center items-center py-1 rounded pr-5 px-3 bg-darkBlue" : "flex text-darkBlue bg-white self-center items-center py-1 rounded pr-5 px-3"}><FaSistrix/> 
-          <input className={themeValue ? 'pl-3 border-none outline-none bg-darkBlue' : 'bg-white pl-3 border-none outline-none'} type="search" placeholder="Search for a country"></input></span>
+          <input onChange={event => setSearch(event.target.value)} className={themeValue ? 'pl-3 border-none outline-none bg-darkBlue' : 'bg-white pl-3 border-none outline-none'} type="search" placeholder="Search for a country"></input></span>
         </div>
         {country && <CountryHome  themeValue={themeValue} country={country}/>}
     </div>
