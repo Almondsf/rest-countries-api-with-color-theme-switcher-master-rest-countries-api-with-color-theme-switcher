f161bb6e6ec898d23d26f170e0bfc329b59b5485
Oluwafemi <oloyedeoluwafemi22@gmail.com>
2022-08-01 05:19:04 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,6 +32,15 @@ export const Home = ({ themeValue, data}) => {
       <div className="flex justify-between drop-shadow rounded">
         <span className={themeValue ? "flex self-center items-center py-1 rounded pr-5 px-3 bg-darkBlue" : "flex text-darkBlue bg-white self-center items-center py-1 rounded pr-5 px-3"}><FaSistrix/> 
         <input onChange={event => setSearch(event.target.value)} className={themeValue ? 'pl-3 border-none outline-none bg-darkBlue' : 'bg-white pl-3 border-none outline-none'} type="search" placeholder="Search for a country"></input></span>
+        <span className="mr-[100px]"><select className={themeValue ? "flex self-center items-center py-1 rounded outline-none pr-5 px-3 bg-darkBlue" : "flex outline-none text-darkBlue bg-white self-center items-center py-1 rounded pr-5 px-3"} name="countries" id="">
+          <option value="volvo">All</option>
+          <option value="volvo">Africa</option>
+          <option value="saab">America</option>
+          <option value="mercedes">Asia</option>
+          <option value="audi">Europe</option>
+          <option value="volvo">Oceania</option>
+        </select>
+        </span>
       </div>
  
       {country && <CountryHome  themeValue={themeValue} country={country} search={search} />}
