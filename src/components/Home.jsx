5c22634ee46c0f71f1e5fc62115be970930014b0
Oluwafemi <oloyedeoluwafemi22@gmail.com>
2022-08-02 00:52:19 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,7 +20,8 @@ export const Home = ({ themeValue, data}) => {
     }, [data])
     
 
-    const [search, setSearch] = useState()
+    const [search, setSearch] = useState(null)
+    const [region, setRegion] = useState(null)
     
     
   return (
@@ -28,22 +29,22 @@ export const Home = ({ themeValue, data}) => {
 
         {loading && <Spinner themeValue={themeValue}/>}
     {
-      !loading && <div className='mx-6 mt-2 py-2'>
+      !loading && <div className='mx-6 mt-2 py-5'>
       <div className="md:flex md:justify-between drop-shadow rounded">
-        <span className={themeValue ? "flex self-center items-center py-1 my-5 rounded pr-5 px-3 bg-darkBlue " : "flex text-darkBlue bg-white self-center items-center py-1 rounded pr-5 my-5 px-3"}><FaSistrix/> 
+        <span className={themeValue ? "flex self-center items-center py-1 rounded md:pr-5 px-3 bg-darkBlue " : "flex text-darkBlue bg-white self-center items-center py-1 rounded md:pr-5 px-3"}><FaSistrix/> 
         <input onChange={event => setSearch(event.target.value)} className={themeValue ? 'md:pl-3 border-none outline-none bg-darkBlue' : 'bg-white md:pl-3 border-none outline-none'} type="search" placeholder="Search for a country"></input></span>
-        <span className="md:mr-[100px]"><select className={themeValue ? "flex self-center items-center py-1 rounded outline-none pr-5 px-3 bg-darkBlue" : "flex outline-none text-darkBlue bg-white self-center items-center py-1 rounded pr-5 px-3"} name="countries" id="">
-          <option value="volvo">All</option>
-          <option value="volvo">Africa</option>
-          <option value="saab">America</option>
-          <option value="mercedes">Asia</option>
-          <option value="audi">Europe</option>
-          <option value="volvo">Oceania</option>
+        <span className=""><select onChange={event => setRegion(event.target.value)} className={themeValue ? "flex self-center items-center py-1 rounded outline-none pr-5 px-3 bg-darkBlue" : "flex outline-none text-darkBlue bg-white self-center items-center py-1 rounded pr-5 px-3"} name="countries" id="">
+          <option>Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
         </select>
         </span>
       </div>
  
-      {country && <CountryHome  themeValue={themeValue} country={country} search={search} />}
+      {country && <CountryHome  themeValue={themeValue} country={country} search={search} region={region}/>}
   </div>
     }
     </>
