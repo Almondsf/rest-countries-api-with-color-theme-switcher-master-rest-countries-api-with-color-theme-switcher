877e27ea2e16bc136d54ea153f940eccae3c02b5
Oluwafemi <oloyedeoluwafemi22@gmail.com>
2022-08-01 05:26:19 +0100

    commit

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -2,7 +2,7 @@
 
 export const Head = ({toggleFunc, themeValue}) => {
   return (
-    <div className={themeValue ? 'bg-darkBlue drop-shadow-md flex justify-between px-10 py-3 font-bold font-Nunito' :'drop-shadow-md border-b flex justify-between px-10 py-3 font-bold font-Nunito'}>
+    <div className={themeValue ? 'cursor-pointer bg-darkBlue drop-shadow-md flex justify-between px-10 py-3 font-bold font-Nunito' :'drop-shadow-md border-b cursor-pointer flex justify-between px-10 py-3 font-bold font-Nunito'}>
         <h2>Where in the world?</h2>
         <h2 onClick={toggleFunc}><span></span>Dark Mode</h2>
     </div>
