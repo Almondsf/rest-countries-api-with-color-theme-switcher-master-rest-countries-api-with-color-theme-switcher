c0a9c90195ffb46426465ccbb5ea90bfc421c1ca
Oluwafemi <oloyedeoluwafemi22@gmail.com>
2022-08-24 16:24:12 +0100

    first commit

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,4 +1,4 @@
-import {useParams} from 'react-router-dom'
+import {useParams, useHistory} from 'react-router-dom'
 import useFetch from './useFetch'
 import Spinner from "./Spinner"
 
@@ -6,10 +6,11 @@ import Spinner from "./Spinner"
 export const Detail = ({themeValue}) => {
   const {callingCodes} = useParams()
   const{country, loading} = useFetch("https://restcountries.com/v2/callingcode/" + callingCodes)
+  const history = useHistory()
   return (
     <div>
     {loading && <Spinner themeValue={themeValue}/>}
-    {loading || <button>Back</button>}
+    {loading || <button className={themeValue ? ' h-2/3  drop-shadow-lg bg-darkBlue rounded' : 'h-2/3 flex drop-shadow-lg bg-lwhite py-1 px-1 rounded' } onClick={() => history.go(-1)}>Back</button>}
       {country && (
         <div>{country.map((cstuff) =>(
           <div>{cstuff.name}</div>
