3f50e41ed1b17652bb69e4b82ee1016792671109
Oluwafemi <oloyedeoluwafemi22@gmail.com>
2022-08-25 09:26:12 +0100

    first commit

--- src/components/CountryHome.jsx
+++ src/components/CountryHome.jsx
@@ -5,7 +5,7 @@ export const CountryHome = ({ themeValue,country, search, region}) => {
       {country?.filter((cstuff) => 
         search || region ? (search && cstuff?.name.toLowerCase().includes(search)) || (region && cstuff?.region === region) : cstuff 
       ).map((cstuff) =>(
-      <Link key={cstuff.numericCode} to={`/country/${cstuff.callingCodes}`}>
+      <Link key={cstuff.numericCode} to={`/country/${cstuff.name}`}>
           <div className={themeValue ? 'hover:scale-90 hover:delay-75 hover:ease-out md:h-5/6 mx-5 h-2/3 my-6 flex flex-col  drop-shadow-lg bg-darkBlue rounded' : ' mx-5 md:h-5/6 h-2/3 flex my-6 flex-col  drop-shadow-lg bg-lwhite rounded' }>
             <span className='rounded-t md:h-[250px] h-[220px]'><img className='md:h-[180px] h-[200px] w-full rounded-t' src={cstuff.flags.png} alt={cstuff.name.official}/></span>
           <div className='flex flex-col justify-center pl-3 md:h-2/3 pb-4 flex-wrap text-sm'>
--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -4,8 +4,8 @@ import Spinner from "./Spinner"
 import { BiArrowBack } from "react-icons/bi";
 
 export const Detail = ({themeValue}) => {
-  const {callingCodes} = useParams()
-  const{country, loading} = useFetch("https://restcountries.com/v2/callingcode/" + callingCodes)
+  const {name} = useParams()
+  const{country, loading} = useFetch("https://restcountries.com/v2/name/" + name)
   const history = useHistory()
   return (
     <div>
@@ -13,7 +13,30 @@ export const Detail = ({themeValue}) => {
     {loading || <button className={themeValue ? ' drop-shadow-lg bg-darkBlue rounded text-sm flex mt-1 px-2 py-1' : 'text-sm mt-1 flex drop-shadow-lg bg-lwhite py-1 px-2 rounded' } onClick={() => history.go(-1)}><span className='mt-1 mr-1'><BiArrowBack/></span>Back</button>}
       {country && (
         <div>{country.map((cstuff) =>(
-          <div>{cstuff.name}</div>
+          <div>
+          <img className='w-[450px] h-[350px]' src={cstuff.flag} alt={cstuff.name}/>
+          <div>
+            <h1>{cstuff.name}</h1>
+            <div className='flex'>
+              <div>
+                <ul>
+                <li>Native Name: {cstuff.nativeName}</li>
+                <li>Population: {cstuff.population}</li>
+                <li>Region: {cstuff.region}</li>
+                <li>Sub Region: {cstuff.subregion}</li>
+                <li>Capital: {cstuff.capital}</li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                <li>Top Level Domain: {cstuff.topLevelDomain}</li>
+                <li>Currencies: {cstuff.currency}</li>
+                <li>Languages: {cstuff.lang}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          </div>
         ))}</div>
       )}
     </div>
